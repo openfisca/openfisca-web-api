198e3eda1562fd919bb0a38b919a7d760daef83b
Christophe Benz <christophe.benz@data.gouv.fr>
2015-01-30 16:42:06 +0100

    Factor WSGI app loading in tests.common

--- openfisca_web_api/tests/test_map_to_swagger.py
+++ openfisca_web_api/tests/test_map_to_swagger.py
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
 
+
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014, 2015 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import unittest
 
-import os
-
-from openfisca_web_api import environment, model
-from paste.deploy import appconfig
-from openfisca_web_api.controllers.swagger import (
+from . import common
+from .. import model
+from ..controllers.swagger import (
     map_path_base_to_swagger,
     map_type_to_swagger,
     map_parameters_to_swagger,
@@ -17,9 +38,7 @@ from openfisca_web_api.controllers.swagger import (
 class TestSwagger(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        ini_file_path = os.path.join(os.path.dirname(__file__), '../../development-france.ini')
-        site_conf = appconfig('config:' + os.path.abspath(ini_file_path) + '#main')
-        environment.load_environment(site_conf.global_conf, site_conf.local_conf)
+        common.get_or_load_app()
 
     def test_map_path_base_to_swagger_withour_url(self):
         expected = {
