c2cd4d48ac2dade49672fd85f38b2079d6b16c01
Christophe Benz <christophe.benz@data.gouv.fr>
2015-01-30 16:42:06 +0100

    Factor WSGI app loading in tests.common

---
+++ openfisca_web_api/tests/common.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014, 2015 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import pkg_resources
+
+from paste.deploy import loadapp
+
+from .. import environment
+
+
+app = None
+CONF_FILE_NAME = 'development-france.ini'
+
+
+def get_or_load_app():
+    global app
+    if app is None:
+        pkg_root_dir = pkg_resources.get_distribution('OpenFisca-Web-API').location
+        conf_file_path = os.path.join(pkg_root_dir, CONF_FILE_NAME)
+        app = loadapp(u'config:{}#main'.format(conf_file_path))
+    return app
