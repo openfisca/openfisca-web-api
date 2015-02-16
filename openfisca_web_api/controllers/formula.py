e9cca4ab71a315954ac49a01f788e13aaba29817
Christophe Benz <christophe.benz@data.gouv.fr>
2015-02-16 15:44:31 +0100

    Remove reform related params to formula endpoint

--- openfisca_web_api/controllers/formula.py
+++ openfisca_web_api/controllers/formula.py
@@ -30,7 +30,7 @@ import collections
 import datetime
 
 import numpy as np
-from openfisca_core import periods, reforms, simulations
+from openfisca_core import periods, simulations
 
 from .. import contexts, conv, model, wsgihelpers
 
@@ -43,62 +43,48 @@ def api1_formula(req):
     ctx = contexts.Ctx(req)
     headers = wsgihelpers.handle_cross_origin_resource_sharing(ctx)
 
-    assert req.method == 'GET', req.method
+    tax_benefit_system = model.tax_benefit_system
+
+    requested_column, error = conv.pipe(
+        conv.cleanup_line,
+        conv.test_in(tax_benefit_system.column_by_name),
+        conv.function(lambda column_name: tax_benefit_system.column_by_name[column_name]),
+        conv.test(lambda column: column.formula_class is not None, error = N_(u"Variable is not a formula")),
+        conv.not_none,
+        )(req.urlvars.get('name'), state = ctx)
+    if error is not None:
+        return wsgihelpers.respond_json(ctx,
+            collections.OrderedDict(sorted(dict(
+                apiVersion = 1,
+                error = collections.OrderedDict(sorted(dict(
+                    code = 400,  # Bad Request
+                    errors = [conv.jsonify_value(error)],
+                    message = ctx._(u'Invalid formula name in request URL'),
+                    ).iteritems())),
+                method = req.script_name,
+                params = req.body,
+                url = req.url.decode('utf-8'),
+                ).iteritems())),
+            headers = headers,
+            )
 
     params = req.GET
-    inputs = dict(
-        context = params.get('context'),
-        name = req.urlvars.get('name'),
-        reforms = params.getall('reform'),
+    inputs = dict(params)
+    fields = dict(
+        (column.name, column.input_to_dated_python)
+        for column in tax_benefit_system.column_by_name.itervalues()
         )
-    str_to_reforms = conv.make_str_to_reforms()
-    data, errors = conv.struct(
-        dict(
-            context = conv.noop,  # For asynchronous calls
-            name = conv.noop,  # Real conversion is done once tax-benefit system is known.
-            reforms = str_to_reforms,
-            ),
-        default = conv.noop,  # Do not drop other fields for other converter.
-        )(inputs, state = ctx)
-
-    if errors is None:
-        country_tax_benefit_system = model.tax_benefit_system
-        tax_benefit_system = reforms.compose_reforms(
-            base_tax_benefit_system = country_tax_benefit_system,
-            build_reform_list = [model.build_reform_function_by_key[reform_key] for reform_key in data['reforms']],
-            ) if data['reforms'] is not None else country_tax_benefit_system
-        data, errors = conv.struct(
-            dict(
-                name = conv.pipe(
-                    conv.empty_to_none,
-                    conv.test_in(tax_benefit_system.column_by_name),
-                    conv.function(lambda column_name: tax_benefit_system.column_by_name[column_name]),
-                    conv.test(
-                        lambda column: column.formula_class is not None,
-                        error = N_(u"Variable is not a formula"),
-                        ),
-                    conv.not_none,
-                    ),
-                ),
-            default = conv.noop,  # Do not drop other fields for other converter.
-            )(data, state = ctx)
-
-    if errors is None:
-        fields = dict(
-            (column.name, column.input_to_dated_python)
-            for column in tax_benefit_system.column_by_name.itervalues()
-            )
-        fields['period'] = conv.pipe(
-            periods.json_or_python_to_period,
-            conv.default(periods.period('month', datetime.date.today())),
-            conv.function(lambda period: period.offset('first-of')),
-            )
-        fields_data, errors = conv.struct(
+    fields['period'] = conv.pipe(
+        periods.json_or_python_to_period,
+        conv.default(periods.period('month', datetime.date.today())),
+        conv.function(lambda period: period.offset('first-of')),
+        )
+    data, errors = conv.pipe(
+        conv.struct(
             fields,
-            default = 'drop',
             drop_none_values = True,
-            )(inputs, state = ctx)
-
+            ),
+        )(params, state = ctx)
     if errors is not None:
         return wsgihelpers.respond_json(ctx,
             collections.OrderedDict(sorted(dict(
@@ -116,7 +102,7 @@ def api1_formula(req):
             headers = headers,
             )
 
-    period = fields_data.pop('period')
+    period = data.pop('period')
     simulation = simulations.Simulation(
         debug = False,
         period = period,
@@ -139,18 +125,18 @@ def api1_formula(req):
             holder = persons.get_or_new_holder(entity.role_for_person_variable_name)
             holder.set_array(period, np.array([0]))
     # Inject all variables from query string into arrays.
-    for column_name, value in fields_data.iteritems():
+    for column_name, value in data.iteritems():
         column = tax_benefit_system.column_by_name[column_name]
         entity = simulation.entity_by_key_plural[column.entity_key_plural]
         holder = entity.get_or_new_holder(column_name)
         holder.set_array(period, np.array([value], dtype = column.dtype))
 
-    requested_dated_holder = simulation.compute(data['name'].name)
+    requested_dated_holder = simulation.compute(requested_column.name)
 
     return wsgihelpers.respond_json(ctx,
         collections.OrderedDict(sorted(dict(
             apiVersion = 1,
-            context = data['context'],
+            # context = data['context'],
             method = req.script_name,
             params = inputs,
             url = req.url.decode('utf-8'),
