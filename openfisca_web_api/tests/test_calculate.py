24beb291cbf62a6aa1126932074cd0613eb18148
Christophe Benz <christophe.benz@data.gouv.fr>
2015-04-15 11:08:18 +0200

    wip

--- openfisca_web_api/tests/test_calculate.py
+++ openfisca_web_api/tests/test_calculate.py
@@ -23,12 +23,45 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import copy
 import json
 
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_in, assert_is_instance
 from webob import Request
 
 from . import common
+
+
+test_case = {
+    'scenarios': [
+        {
+            'test_case': {
+                'familles': [
+                    {
+                        'parents': ['ind0', 'ind1'],
+                        },
+                    ],
+                'foyers_fiscaux': [
+                    {
+                        'declarants': ['ind0', 'ind1'],
+                        },
+                    ],
+                'individus': [
+                    {'id': 'ind0', 'sali': 15000},
+                    {'id': 'ind1'},
+                    ],
+                'menages': [
+                    {
+                        'conjoint': 'ind1',
+                        'personne_de_reference': 'ind0',
+                        },
+                    ],
+                },
+            'period': '2013',
+            },
+        ],
+    'variables': ['revdisp'],
+    }
 
 
 def setup_module(module):
@@ -53,36 +86,6 @@ def test_calculate_with_invalid_body():
 
 
 def test_calculate_with_test_case():
-    test_case = {
-        'scenarios': [
-            {
-                'test_case': {
-                    'familles': [
-                        {
-                            'parents': ['ind0', 'ind1'],
-                            },
-                        ],
-                    'foyers_fiscaux': [
-                        {
-                            'declarants': ['ind0', 'ind1'],
-                            },
-                        ],
-                    'individus': [
-                        {'id': 'ind0', 'sali': 15000},
-                        {'id': 'ind1'},
-                        ],
-                    'menages': [
-                        {
-                            'conjoint': 'ind1',
-                            'personne_de_reference': 'ind0',
-                            },
-                        ],
-                    },
-                'period': '2013',
-                },
-            ],
-        'variables': ['revdisp'],
-        }
     req = Request.blank(
         '/api/1/calculate',
         body = json.dumps(test_case),
@@ -91,3 +94,23 @@ def test_calculate_with_test_case():
         )
     res = req.get_response(common.app)
     assert_equal(res.status_code, 200)
+    res_json = json.loads(res.body)
+    assert_is_instance(res_json, dict)
+    assert_in('value', res_json)
+
+
+def test_calculate_with_trace():
+    test_case_with_trace = copy.deepcopy(test_case)
+    test_case_with_trace['trace'] = True
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case_with_trace),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 200)
+    res_json = json.loads(res.body)
+    assert_is_instance(res_json, dict)
+    assert_in('value', res_json)
+    assert_in('tracebacks', res_json)
