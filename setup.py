498a36b006b01fb617419e2d41b34fbd84677f83
Christophe Benz <christophe.benz@data.gouv.fr>
2015-09-07 19:28:55 +0200

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -24,36 +24,27 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-"""Web API for OpenFisca -- a versatile microsimulation free software"""
-
-
 from setuptools import setup, find_packages
-
-
-classifiers = """\
-Development Status :: 2 - Pre-Alpha
-Environment :: Web Environment
-License :: OSI Approved :: GNU Affero General Public License v3
-Operating System :: POSIX
-Programming Language :: Python
-Topic :: Scientific/Engineering :: Information Analysis
-Topic :: Internet :: WWW/HTTP :: WSGI :: Server
-"""
-
-doc_lines = __doc__.split('\n')
 
 
 setup(
     name = 'OpenFisca-Web-API',
-    version = '0.5.dev0',
+    version = '0.5.0',
 
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
-    classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
-    description = doc_lines[0],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+        ],
+    description = u'Web API for OpenFisca',
     keywords = 'api benefit microsimulation server social tax web',
     license = 'http://www.fsf.org/licensing/licenses/agpl-3.0.html',
-    long_description = '\n'.join(doc_lines[2:]),
     url = 'https://github.com/openfisca/openfisca-web-api',
 
     data_files = [
@@ -78,5 +69,4 @@ setup(
         ]},
     packages = find_packages(),
     test_suite = 'nose.collector',
-    zip_safe = False,
     )
