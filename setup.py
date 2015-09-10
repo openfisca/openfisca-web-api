e2d8250bf36a6cc69b76622a35a0e49b528166ab
Christophe Benz <christophe.benz@data.gouv.fr>
2015-09-10 14:45:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,18 +49,25 @@ setup(
 
     data_files = [
         ('share/locale/fr/LC_MESSAGES', ['openfisca_web_api/i18n/fr/LC_MESSAGES/openfisca-web-api.mo']),
+        ('share/openfisca/openfisca-web-api', ['development-france.ini', 'development-tunisia.ini', 'test.ini']),
         ],
     entry_points = {
         'paste.app_factory': 'main = openfisca_web_api.application:make_app',
         },
-    include_package_data = True,
+    extras_require = {
+        'dev': [
+            'PasteScript',
+            ],
+        'test': [
+            'nose',
+            ],
+        },
     install_requires = [
         'Babel >= 0.9.4',
         'Biryani >= 0.10.4',
         'OpenFisca-Core >= 0.5.0',
         'OpenFisca-Parsers >= 0.5dev',
         'PasteDeploy',
-        'PasteScript',
         'WebError >= 0.10',
         'WebOb >= 1.1',
         ],
