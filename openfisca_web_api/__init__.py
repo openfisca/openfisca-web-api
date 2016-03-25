173d604f613e24e71dc8972895ecf6e6c0004f65
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-03-25 17:40:06 +0100

    Add start method

--- openfisca_web_api/__init__.py
+++ openfisca_web_api/__init__.py
@@ -2,3 +2,8 @@
 
 
 conf = {}  # Dictionary updated by environment.load_environment
+
+def start(rel_path_to_config_file = 'resources/default-config.ini'):
+	import os
+	path_to_config_file = os.path.join(os.path.dirname(__file__), rel_path_to_config_file)
+	os.system('paster serve ' + path_to_config_file)
