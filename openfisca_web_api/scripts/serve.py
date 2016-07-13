c6b8ff0f5c8b67dd6d48ccfe8c82b98d33b979a6
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-07-13 21:27:40 +0200

    Manage case where api installed with --editable

--- openfisca_web_api/scripts/serve.py
+++ openfisca_web_api/scripts/serve.py
@@ -15,6 +15,13 @@ port = 2000
 
 def main():
     conf_file_path = os.path.join(sys.prefix, 'share', 'openfisca', 'openfisca-web-api', 'development-france.ini')
+
+    # If openfisca_web_api has been installed with --editable
+    if not os.path.isfile(conf_file_path):
+        import pkg_resources
+        api_sources_path = pkg_resources.get_distribution("openfisca_web_api").location
+        conf_file_path = os.path.join(api_sources_path, 'development-france.ini')
+
     fileConfig(conf_file_path)
     application = loadapp('config:{}'.format(conf_file_path))
     httpd = make_server(hostname, port, application)
