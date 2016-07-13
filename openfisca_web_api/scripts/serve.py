84783cdcdd52108df359cbe2add8c41b92b97e0b
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-07-13 22:03:02 +0200

    Allow port to be changed

--- openfisca_web_api/scripts/serve.py
+++ openfisca_web_api/scripts/serve.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
-
 import os
 import sys
+import argparse
 from logging.config import fileConfig
 from wsgiref.simple_server import make_server
 
 from paste.deploy import loadapp
 
 
-hostname = 'localhost'
-port = 2000
-
-
 def main():
+    parser = argparse.ArgumentParser(description = __doc__)
+    parser.add_argument('-p', '--port', action = 'store', default = 2000, help = "port to serve on")
+    args = parser.parse_args()
+
+    port = int(args.port)
+    hostname = 'localhost'
     conf_file_path = os.path.join(sys.prefix, 'share', 'openfisca', 'openfisca-web-api', 'development-france.ini')
 
     # If openfisca_web_api has been installed with --editable
