e3ce6e5a9784c95c48a742728f586b6f1514949d
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-11-22 16:02:03 +0100

    Update version and changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Web-API',
-    version = '2.0.3',
+    version = '3.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
