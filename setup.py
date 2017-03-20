6b7fd33e07cf4fa0bb3f132431ddeb182e726ca9
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-03-20 17:36:51 +0100

    Upgrade core

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'Babel >= 0.9.4',
         'Biryani[datetimeconv] >= 0.10.4',
         'numpy >= 1.11',
-        'OpenFisca-Core[parsers] >= 7.1.0, < 8.0',
+        'OpenFisca-Core[parsers] >= 8.0, < 9.0',
         'PasteDeploy',
         'WebError >= 0.10',
         'WebOb >= 1.1',
