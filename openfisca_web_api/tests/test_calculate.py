920344ee85322a19242b7dd842919197fd71b939
Christophe Benz <christophe.benz@data.gouv.fr>
2017-04-14 14:30:16 +0200

    Add tests

--- openfisca_web_api/tests/test_calculate.py
+++ openfisca_web_api/tests/test_calculate.py
@@ -342,10 +342,12 @@ def test_calculate_with_wrong_input_variable_period():
                     'foyers_fiscaux': [
                         {
                             'declarants': ['ind0', 'ind1'],
+                            # rfr is defined for a year, its value can't be set for a month
+                            'rfr': {"2013-01": 15000},
                             },
                         ],
                     'individus': [
-                        {'id': 'ind0', 'enfant_a_charge': {"2013-01": 15000}},
+                        {'id': 'ind0'},
                         {'id': 'ind1'},
                         ],
                     'menages': [
@@ -369,4 +371,557 @@ def test_calculate_with_wrong_input_variable_period():
     res = req.get_response(common.app)
     assert_equal(res.status_code, 400, res.body)
     res_body_json = json.loads(res.body)
-    assert_in(u'ValueError: Unable to set a value for variable', res_body_json['error']['message'], res.body)
+    assert_in(
+        u'ValueError: Unable to set a value for variable rfr for month-long period 2013-01',
+        res_body_json['error']['message'],
+        res.body,
+        )
+
+
+def test_calculate_with_wrong_variable_period():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0'},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013-01',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(
+        u'ValueError: Unable to compute variable revenu_disponible for period 2013-01',
+        res_body_json['error']['message'],
+        res.body,
+        )
+
+
+def test_calculate_with_wrong_input_variable():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0', 'i_do_not_exist': 42},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(
+        u'VariableNotFound: You tried to calculate or to set a value for variable \'i_do_not_exist\'',
+        res_body_json['error']['message'],
+        res.body,
+        )
+
+
+def test_calculate_with_wrong_variable():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0'},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['i_do_not_exist'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(
+        u'VariableNotFound: You tried to calculate or to set a value for variable \'i_do_not_exist\'',
+        res_body_json['error']['message'],
+        res.body,
+        )
+
+
+def test_calculate_with_wrong_input_period_string():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0', 'enfant_a_charge': {'i_am_a_wrong_period': 42}},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(u'ValueError: Invalid period i_am_a_wrong_period', res_body_json['error']['message'], res.body)
+
+
+def test_calculate_with_wrong_input_period_none():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0', 'enfant_a_charge': {None: 42}},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(u'ValueError: Invalid period null', res_body_json['error']['message'], res.body)
+
+
+def test_calculate_with_wrong_period_string():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0'},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': 'i_am_a_wrong_period_string',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(u'ValueError: Invalid period i_am_a_wrong_period_string', res_body_json['error']['message'], res.body)
+
+
+def test_calculate_with_wrong_period_list():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0'},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': ['i_am_a_wrong_period_list'],
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(
+        u'ValueError: Invalid period',
+        res_body_json['error']['message'],
+        res.body,
+        )
+
+
+def test_calculate_with_input_variable_set_to_wrong_entity():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            'salaire_imposable': 42,
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0'},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(
+        u'ValueError: Variable salaire_imposable is defined for entity individu.',
+        res_body_json['error']['message'],
+        res.body,
+        )
+
+
+def test_calculate_with_variable_with_set_input_and_inconsistent_input_data():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0'},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            'loyer': {
+                                '2013': 12000,
+                                'month:2013-01:12': 1000,
+                                },
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(
+        u'ValueError: Inconsistent input: variable loyer has already been set for all months '
+        u'contained in period 2013, and value [ 12000.] provided for 2013 doesn\'t match the total ([ 999.99609375]).',
+        res_body_json['error']['message'],
+        res.body,
+        )
+
+
+def test_calculate_with_wrong_entity_name():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'i_am_a_wrong_entity': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0'},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(u'ValueError: Invalid entity name: i_am_a_wrong_entity', res_body_json['error']['message'], res.body)
+
+
+def test_calculate_with_wrong_value_type():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': 'ind0', 'salaire_imposable': [15000]},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(
+        u'ValueError: Invalid value [15000] for variable salaire_imposable.',
+        res_body_json['error']['message'], res.body
+        )
+
+
+def test_calculate_with_wrong_id_type():
+    test_case = {
+        'scenarios': [
+            {
+                'test_case': {
+                    'familles': [
+                        {
+                            'parents': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'foyers_fiscaux': [
+                        {
+                            'declarants': ['ind0', 'ind1'],
+                            },
+                        ],
+                    'individus': [
+                        {'id': {'k': 'ind0'}},
+                        {'id': 'ind1'},
+                        ],
+                    'menages': [
+                        {
+                            'conjoint': 'ind1',
+                            'personne_de_reference': 'ind0',
+                            },
+                        ],
+                    },
+                'period': '2013',
+                },
+            ],
+        'variables': ['revenu_disponible'],
+        }
+    req = Request.blank(
+        '/api/1/calculate',
+        body = json.dumps(test_case),
+        headers = (('Content-Type', 'application/json'),),
+        method = 'POST',
+        )
+    res = req.get_response(common.app)
+    assert_equal(res.status_code, 400, res.body)
+    res_body_json = json.loads(res.body)
+    assert_in(u'ValueError: Invalid id in entity', res_body_json['error']['message'], res.body)
