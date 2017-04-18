1bf3eaeaf46582a5bff48c560444c3a0ec330d6f
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-04-18 17:36:14 +0200

    [TO REVERT] Test tag creation in CI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Web-API',
-    version = '4.0.0',
+    version = '4.0.0.dev0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
