7ae4a973be70d4dd029a497cbe44b893c1a05375
Christophe Benz <christophe.benz@gmail.com>
2017-04-19 19:05:47 +0200

    Install requirements from ops

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -7,7 +7,7 @@ cd /home/openfisca/new-api/openfisca-web-api
 git fetch origin rewrite
 git checkout origin/rewrite --force
 pip install --editable . --upgrade
-pip install openfisca_france --upgrade
+pip install --requirement /home/openfisca/openfisca-ops/auto-update-pip-packages/requirements.txt --upgrade
 # The current user must have been specifically allowed to run the next command
 # Use the visudo command to do so
 sudo systemctl restart openfisca-web-api-new.service
