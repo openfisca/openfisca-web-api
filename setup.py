30178544f0723ed0fcf3ca3d7515604317fbc833
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-06-13 19:00:11 +0200

    Upgrade core compatibility

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Web-API',
-    version = '5.1.2',
+    version = '5.1.3',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
@@ -54,7 +54,7 @@ setup(
         'Babel >= 0.9.4',
         'Biryani[datetimeconv] >= 0.10.4',
         'numpy >= 1.11, < 1.13',
-        'OpenFisca-Core[parsers] >= 10.0.1, < 14.0',
+        'OpenFisca-Core[parsers] >= 10.0.1, < 15.0',
         'PasteDeploy',
         'WebError >= 0.10',
         'WebOb >= 1.1',
