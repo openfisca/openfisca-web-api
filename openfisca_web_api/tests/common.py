9930161df3feb033b5b43ad714aecd325186f392
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-07-31 19:33:12 +0200

    Remove heavy unit test
    Not worth the complexity

--- openfisca_web_api/tests/common.py
+++ openfisca_web_api/tests/common.py
@@ -9,7 +9,6 @@ from paste.deploy import loadapp
 
 app = None
 CONF_FILE_NAME = 'test.ini'
-CONF_TRACKING_FILE_NAME = 'test_tracking.ini'
 
 
 def get_or_load_app():
@@ -17,14 +16,5 @@ def get_or_load_app():
     if app is None:
         pkg_root_dir = pkg_resources.get_distribution('OpenFisca-Web-API').location
         conf_file_path = os.path.join(pkg_root_dir, CONF_FILE_NAME)
-        app = loadapp(u'config:{}#main'.format(conf_file_path))
-    return app
-
-
-def get_or_load_tracked_app():
-    global app
-    if app is None:
-        pkg_root_dir = pkg_resources.get_distribution('OpenFisca-Web-API').location
-        conf_file_path = os.path.join(pkg_root_dir, CONF_TRACKING_FILE_NAME)
         app = loadapp(u'config:{}#main'.format(conf_file_path))
     return app
