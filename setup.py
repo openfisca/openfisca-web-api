a13179dcbdaeb3409b9b14ac9d2081708db4a7c1
Florian Pagnoux <florian.pagnoux@gmail.com>
2017-11-06 23:21:56 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Web-API',
-    version = '7.1.0',
+    version = '7.2.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
