20bab3fe983a87c4b93a14a95aebb54e66cbc277
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-04-02 17:55:15 -0400

    Upgrade dependencies

--- setup.py
+++ setup.py
@@ -56,8 +56,7 @@ setup(
     install_requires = [
         'Babel >= 0.9.4',
         'Biryani[datetimeconv] >= 0.10.4',
-        'numpy >= 1.11, < 1.13',
-        'OpenFisca-Core >= 22.0.1, < 23.0',
+        'OpenFisca-Core >= 22.0.3, < 23.0',
         'PasteDeploy',
         'WebError >= 0.10',
         'WebOb >= 1.1',
